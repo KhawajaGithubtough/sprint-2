38daaca59bbcadade156ca3f2399c6e41f169e6b
test <sadiq.hassan@gmail.com>
2022-11-30 10:14:24 +1100

    task object

--- script.js
+++ script.js
@@ -1,89 +1,18 @@
-class ValidateForm {
-    constructor(name, description, assignedTo, duedate, form) {
-        this.name = name;     // initialize variables in constuctor
-        this.dscription = description;   
-        this.assignedTo = assignedTo;   
-        this.duedate = duedate;
-        this.form = form;
-    }
+function saveTask()
+            {
+                let taskdstatus=document.querySelector('input[name="com"]:checked').value
+                let workstatus=document.getElementById('ip').value
+                let workstatus1=document.getElementById('bp').value
+                let taskstatus=document.getElementById('ct').value
+                var des=document.getElementById('des').value
 
-    // method to validate name input
-    validateName(name) {
-        const regName = /^[A-Z]'?[-a-zA-Z]+$/
-
-        let isNameValid = false;
-        let name_z = name.value.trim(); // remove leading and trailing white spaces
-        let isNameHasValidLength = name_z.length < 0 || name_z.length > 8 ? false : true;
-
-        // vallidating length, empty and regex pattern
-        if( !(name_z === '') && isNameHasValidLength && (regName.test(name_Z)) ){
-            isNameValid = true;
-        }
-        return isNameValid;
-    }
-
-    validateDueDate(duedate) {
-
-        let isDueDateValid = false;
-        let dueDate_z = duedate.value.trim(); // input value of Phone input field
-        let isDueDateValidLength = dueDate_z.length < 0 || phone_z.length > 10 ? false : true; // validating date length field 
-
-        const dateReg = /^\d{2}([./-])\d{2}\1\d{4}$/
-
-        // validating date for empty value and length and regex pattern
-
-
-        if( !(dueDate_z === '') && isDueDateValidLength && dateReg.test(duedate)) {
-            isDueDateValid = true;
-        }
-        return isDueDateValid;
-    }
-
-    validateDescription(description) {
-
-            let value = description.value;
-             // checking if description is empty or contain new lines 
-            if (value == null || value === '' || value.indexOf('\n') > 0) {
-                return false;
+                let task={
+                    taskdonestatus:taskdstatus,
+                    workstatus:workstatus,
+                    workstatus1:workstatus1,
+                    taskstatus:taskstatus,
+                    description:des
+                }
+                console.log(task)
+                localStorage.setItem("task_data",JSON.stringify(task))
             }
-    }
-
-    validateAssignTo(assignedTo) {
-        const regAssignTo = /^[A-Z]'?[-a-zA-Z]+$/
-
-        let aasignTo = false;
-        let assignTo_z = assignedTo.value.trim(); // remove leading and trailing white spaces
-        let isassignToHasValidLength = assignTo_z.length < 0 || assignTo_z.length > 8 ? false : true;
-
-        // vallidating length, empty and regex pattern
-        if( !(name_z === '') && isassignToHasValidLength && (regAssignTo.test(regAssignTo)) ){
-            isNameValid = true;
-        }
-        return isNameValid;
-    }
-
-
-}
-
-let vform = new ValidateForm(document.querySelector('#name'), document.querySelector('#description'), document.querySelector('#tname') ,document.querySelector('#date'), document.querySelector('#formV'));
-
-vform.addEventListener('submit', function(e) {
-    let nameValid;
-    let descriptionValid;
-    let assignToValid;
-    let dueDateValid;
-    let submitvalid;
-
-    nameValid = vform.validateName(vform.name);
-    descriptionValid = vform.validateDescription(vform.descriptionValid);
-    assignToValid =vform.validateAssignTo(vform.assignedTo);
-    dueDateValid = vform.validateDueDate(vform.duedate);
-
-
-    submitvalid = nameValid && descriptionValid && assignToValid && dueDateValid ;
-
-    // Prevent default on form submission
-    if (!submitOkay) {
-        e.preventDefault();
-    }
-});
